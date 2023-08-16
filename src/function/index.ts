580b6af5069b0bcf68b5179e0a28d19fe3f0eceb
Keivan-sf <keyvan0082@gmail.com>
2023-08-16 22:04:16 +0330

    chore: export `tryCatch` and `asyncTryCatch`

--- src/function/index.ts
+++ src/function/index.ts
@@ -4,3 +4,5 @@ export { noop } from './noop.ts';
 export { callAll } from './callAll.ts';
 export { debounce } from './debounce.ts';
 export { throttle } from './throttle.ts';
+export { asyncTryCatch } from './asyncTryCatch.ts';
+export { tryCatch } from './tryCatch.ts';
