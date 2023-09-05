1352c627fb62831443410979c6304be8e52dcf27
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-05 23:02:11 +0400

    feat(function): migrate tryCatch to tryOr

--- src/function/index.ts
+++ src/function/index.ts
@@ -4,5 +4,4 @@ export { noop } from './noop.ts';
 export { callAll } from './callAll.ts';
 export { debounce } from './debounce.ts';
 export { throttle } from './throttle.ts';
-export { asyncTryCatch } from './asyncTryCatch.ts';
-export { tryCatch } from './tryCatch.ts';
+export { tryOr } from './tryOr.ts';
